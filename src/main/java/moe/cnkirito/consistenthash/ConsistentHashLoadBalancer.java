7caea64ec4acfb6221bc5bc4003a0332429c4f4a
noahsarkzhang-ts <chuanzhang_ts@163.com>
2019-11-10 22:00:35 +0800

    添加注释

--- src/main/java/moe/cnkirito/consistenthash/ConsistentHashLoadBalancer.java
+++ src/main/java/moe/cnkirito/consistenthash/ConsistentHashLoadBalancer.java
@@ -11,11 +11,27 @@ import java.util.TreeMap;
  */
 public class ConsistentHashLoadBalancer implements LoadBalancer{
 
+    /**
+     *  使用的哈希算法
+     */
     private HashStrategy hashStrategy = new FnvHashStrategy();
 
+    /**
+     *  虚拟结点的个数
+     */
     private final static int VIRTUAL_NODE_SIZE = 10;
+
+    /**
+     *  服务器与虚拟结点的连接符
+     */
     private final static String VIRTUAL_NODE_SUFFIX = "&&";
 
+    /**
+     * 根据请求匹配服务器
+     * @param servers 服务器数组
+     * @param invocation 封装了调用的请求
+     * @return 返回匹配的值
+     */
     @Override
     public Server select(List<Server> servers, Invocation invocation) {
         int invocationHashCode = hashStrategy.getHashCode(invocation.getHashKey());
@@ -24,6 +40,13 @@ public class ConsistentHashLoadBalancer implements LoadBalancer{
         return server;
     }
 
+    /**
+     * 从TreeMap中找出高key最近的服务器，如果没有服务器匹配，
+     * 则返回第一值，形成一个环
+     * @param ring 存储服务器结点的哈希
+     * @param invocationHashCode 封装了调用的请求
+     * @return
+     */
     private Server locate(TreeMap<Integer, Server> ring, int invocationHashCode) {
         // 向右找到第一个 key
         Map.Entry<Integer, Server> locateEntry = ring.ceilingEntry(invocationHashCode);
@@ -34,6 +57,11 @@ public class ConsistentHashLoadBalancer implements LoadBalancer{
         return locateEntry.getValue();
     }
 
+    /**
+     * 构建一个带虚拟结点的哈希环，使用TreeMap结构，底层是红黑树，
+     * @param servers 服务器数组
+     * @return 返回服务器数组的哈希环
+     */
     private TreeMap<Integer, Server> buildConsistentHashRing(List<Server> servers) {
         TreeMap<Integer, Server> virtualNodeRing = new TreeMap<>();
         for (Server server : servers) {
--- src/main/java/moe/cnkirito/consistenthash/KetamaConsistentHashLoadBalancer.java
+++ src/main/java/moe/cnkirito/consistenthash/KetamaConsistentHashLoadBalancer.java
@@ -23,9 +23,22 @@ public class KetamaConsistentHashLoadBalancer implements LoadBalancer {
         }
     }
 
+    /**
+     *  虚拟结点的个数，虚拟结点一般以4个为一组计算，所以数量一般同4的倍数
+     */
     private final static int VIRTUAL_NODE_SIZE = 12;
+
+    /**
+     *  服务器与虚拟结点的连接符
+     */
     private final static String VIRTUAL_NODE_SUFFIX = "-";
 
+    /**
+     * 根据请求匹配服务器
+     * @param servers 服务器数组
+     * @param invocation 封装了调用的请求
+     * @return 返回匹配的值
+     */
     @Override
     public Server select(List<Server> servers, Invocation invocation) {
         long invocationHashCode = getHashCode(invocation.getHashKey());
@@ -34,6 +47,13 @@ public class KetamaConsistentHashLoadBalancer implements LoadBalancer {
         return server;
     }
 
+    /**
+     * 从TreeMap中找出高key最近的服务器，如果没有服务器匹配，
+     * 则返回第一值，形成一个环
+     * @param ring 存储服务器结点的哈希
+     * @param invocationHashCode 封装了调用的请求
+     * @return
+     */
     private Server locate(TreeMap<Long, Server> ring, Long invocationHashCode) {
         // 向右找到第一个 key
         Map.Entry<Long, Server> locateEntry = ring.ceilingEntry(invocationHashCode);
@@ -44,12 +64,20 @@ public class KetamaConsistentHashLoadBalancer implements LoadBalancer {
         return locateEntry.getValue();
     }
 
+    /**
+     * 构建一个带虚拟结点的哈希环，使用TreeMap结构，底层是红黑树，
+     * @param servers 服务器数组
+     * @return 返回服务器数组的哈希环
+     */
     private TreeMap<Long, Server> buildConsistentHashRing(List<Server> servers) {
         TreeMap<Long, Server> virtualNodeRing = new TreeMap<>();
         for (Server server : servers) {
             for (int i = 0; i < VIRTUAL_NODE_SIZE / 4; i++) {
                 byte[] digest = computeMd5(server.getUrl() + VIRTUAL_NODE_SUFFIX + i);
                 for (int h = 0; h < 4; h++) {
+                    // 将digest数组按每四个元素为一组，通过位操作产生一个最大32位的长整数，
+                    // 一个16位的MD5，可以生成4个哈希值，即4个虚拟结点，所以一般将虚拟结点按照
+                    // 4个为一组进行计算。
                     Long k = ((long) (digest[3 + h * 4] & 0xFF) << 24)
                             | ((long) (digest[2 + h * 4] & 0xFF) << 16)
                             | ((long) (digest[1 + h * 4] & 0xFF) << 8)
@@ -62,8 +90,14 @@ public class KetamaConsistentHashLoadBalancer implements LoadBalancer {
         return virtualNodeRing;
     }
 
+    /**
+     * 计算字符串的Ketama哈希值
+     * @param origin key值
+     * @return 哈希值
+     */
     private long getHashCode(String origin) {
         byte[] bKey = computeMd5(origin);
+        // 使用前4个字符进行位运算得到32位的整数。
         long rv = ((long) (bKey[3] & 0xFF) << 24)
                 | ((long) (bKey[2] & 0xFF) << 16)
                 | ((long) (bKey[1] & 0xFF) << 8)
@@ -71,6 +105,11 @@ public class KetamaConsistentHashLoadBalancer implements LoadBalancer {
         return rv;
     }
 
+    /**
+     * 根据key生成16位的MD5摘要，因此digest数组共16位
+     * @param k key
+     * @return MD5摘
+     */
     private static byte[] computeMd5(String k) {
         MessageDigest md5;
         try {
